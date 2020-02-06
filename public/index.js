43eef4d8fbb5947a72e805926021956270049827
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-02-06 12:28:00 -0500

    tinkered around with folder structure and code; for some reason manifest is not registering or appearing upon the inspection of the page and of the app

--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+const fetch = require("node-fetch");
+
 let transactions = [];
 let myChart;
 
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 });
 
 // routes here
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);

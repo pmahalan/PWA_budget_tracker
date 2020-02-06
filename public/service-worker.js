6f6e4dce74385b2cf87b07cbaaa27ee1af29f5b3
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-02-06 12:02:36 -0500

    reinserted the code for the activate portion of the service-worker after finding an example

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,7 +17,7 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cahce-v1";
 
-// when the service work is installed
+// (1) Installing the service worker and skipping the "waiting" period
 self.addEventListener("install", function(evt) {
     evt.waitUntil(
       caches.open(CACHE_NAME).then(cache => {
@@ -46,7 +46,25 @@ self.addEventListener("install", function(evt) {
     self.clients.claim();
   });
 
-// fetch
+// (2) Activating; removing any old cached data.
+self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+
+// (3) Fetching the latest cache!
 self.addEventListener("fetch", function(evt) {
     if (evt.request.url.includes("/api/")) {
       evt.respondWith(

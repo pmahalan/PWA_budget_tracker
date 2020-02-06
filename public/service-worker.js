778403c8c905a5ae6437c49514efa3422d080a62
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-02-06 11:54:05 -0500

    updated the 'files-to-cache' part of my service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,18 @@
 const FILES_TO_CACHE = [
-
+    "/",
+    "/index.html",
+    "/style.css",
+    "/index.js",
+    "/manifest.webmanifest",
+    "/favicon.ico",
+    "/assets/icon-72x72.png",
+    "/assets/icon-96x96.png",
+    "/assets/icon-128x128.png",
+    "/assets/icon-144x144.png",
+    "/assets/icon-152x152.png",
+    "/assets/icon-192x192.png",
+    "/assets/icon-384x384.png",
+    "/assets/icon-512x512.png",
 ];
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cahce-v1";
